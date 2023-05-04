cc2da2e0e7efa6fa0c3f95598b279b11c3b75fc1
Mohammed Faizan Ahmed <faizanzaid789@gmail.com>
2023-05-04 18:36:49 +0530

    created a page

--- index.js
+++ index.js
@@ -7,42 +7,31 @@ const Stripe = require("stripe");
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 const axios = require("axios");
 
-
-
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.post("/api/auth/register", async (req, res) => {
-    const { email, password } = req.body;
-    const customer = await stripe.customers.create({
-        email,
-        description: "My First Test Customer (created for API docs)",
+  const { email, password } = req.body;
+  const customer = await stripe.customers.create({
+    email,
+    description: "My First Test Customer (created for API docs)",
+  });
+
+  axios
+    .post("http://localhost:5000/users", {
+      email,
+      password,
+        stripeId: customer.id,
+    })
+    .then((response) => {
+      console.log(response.data);
     });
 
-    axios.post("http://localhost:5000/users", {
-        email,
-        password,
-    }).then((response) => {
-        console.log(response.data);
-    });
-    
-
-
-
-    res.json(customer);
+  res.json(customer);
 });
 
-
-app.post("/api/checkout_session", async (req, res) => {
-
-
-})
-
-
-
+app.post("/api/checkout_session", async (req, res) => {});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- stripe-with-react/src/App.jsx
+++ stripe-with-react/src/App.jsx
@@ -1,35 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
+  const products = [
+    { name: "Product 1", price: "$10.99" },
+    { name: "Product 2", price: "$15.99" },
+    { name: "Product 3", price: "$20.99" },
+    { name: "Product 4", price: "$25.99" },
+  ];
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="min-h-screen flex flex-col">
+      {/* Navbar */}
+      <nav className="bg-gray-800">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex items-center">
+              <div className="flex-shrink-0">
+                <img className="h-20 w-20" src="/logo.png" alt="Logo" />
+              </div>
+              <div className="hidden md:block">
+                <div className="ml-10 flex items-baseline space-x-4">
+                  <a
+                    href="/"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Home
+                  </a>
+                  <a
+                    href="/"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Products
+                  </a>
+                  <a
+                    href="/"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    About
+                  </a>
+                  <a
+                    href="/"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Contact
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      {/* Product List */}
+      <div className="flex-grow">
+        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            {products.map((product, index) => (
+              <div
+                key={index}
+                className="bg-white overflow-hidden shadow-md hover:shadow-xl cursor-pointer transition rounded-lg "
+              >
+                <div className="px-4 py-5 sm:p-6">
+                  <h3 className="text-lg font-medium mb-2">{product.name}</h3>
+                  <div className="text-gray-500">${product.price}</div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      {/* Footer */}
+      <footer className="bg-gray-800 mt-auto">
+        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <p className="text-center text-gray-400">
+            © 2023 My Website. All rights reserved.
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
