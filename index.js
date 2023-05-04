2592827319fe4eb729fba19c71bd3df7d88a468a
Mohammed Faizan Ahmed <faizanzaid789@gmail.com>
2023-05-04 18:01:48 +0530

    created json server to handle users

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = 3001;
+require("dotenv").config();
+const Stripe = require("stripe");
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+const axios = require("axios");
+
+
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.post("/api/auth/register", async (req, res) => {
+    const { email, password } = req.body;
+    const customer = await stripe.customers.create({
+        email,
+        description: "My First Test Customer (created for API docs)",
+    });
+
+    axios.post("http://localhost:5000/users", {
+        email,
+        password,
+    }).then((response) => {
+        console.log(response.data);
+    });
+    
+
+
+
+    res.json(customer);
+});
+
+
+app.post("/api/checkout_session", async (req, res) => {
+
+
+})
+
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
